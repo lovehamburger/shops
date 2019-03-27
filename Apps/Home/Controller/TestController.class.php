ee3bb4c69fb333b1ac5b6f78acadd61065cf1301
chenlh <243533804@qq.com>
2019-03-27 09:19:27 +0800

    测试

--- Apps/Home/Controller/TestController.class.php
+++ Apps/Home/Controller/TestController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class TestController extends Controller {
     public function index(){
+        echo "11";
    echo  S('test111',wwww); 
    $res = M('User')->select();
 echo D()->getLastSql();  
